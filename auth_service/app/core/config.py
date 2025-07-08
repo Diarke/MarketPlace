66264c861377a3a659145c72cac873eea171c0ae
diar <kerimkul_diar99@mail.ru>
2025-07-08 11:37:15 +0500

    Изменений логики переменных движка

--- auth_service/app/core/config.py
+++ auth_service/app/core/config.py
@@ -2,7 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_ENGINE: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_NAME: str
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
