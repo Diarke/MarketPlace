4a0eff964494b8a787ad036952fd0eeb74828944
diar <kerimkul_diar99@mail.ru>
2025-07-08 11:42:37 +0500

    Исправление ошибки наименования

--- auth_service/app/api/auth.py
+++ auth_service/app/api/auth.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-from app.schemas.user import UserCreate, UserOut, Roles
+from app.schemas.user import UserCreate, UserOut, Roles, Token
 from app.services.auth import register_user, authenticate_user, get_current_user
 from app.core.security import create_access_token
 from app.database.session import get_db
@@ -19,13 +19,13 @@ async def register(user_create: UserCreate, db: AsyncSession = Depends(get_db)):
     return await register_user(user_create, db)
 
 
-@router.post("/login", response_model = UserOut)
+@router.post("/login", response_model = Token)
 async def login(user: UserCreate, db: AsyncSession = Depends(get_db)):
-    user = await authenticate_user(user, db)
+    user = await authenticate_user(user.username, user.password, db)
     if not user:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail = "Неправильные данные")
     access_token = create_access_token(data = {"sub": user.username}, expires_delta = timedelta(minutes = 30))
-    return {"access token": access_token, "token type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get("/user", response_model = UserOut)
