6c834dce6ae145d686f0595eeab93808b63c4329
diar <kerimkul_diar99@mail.ru>
2025-07-08 11:44:44 +0500

    Исправление логики распределения переменных движка

--- auth_service/app/database/session.py
+++ auth_service/app/database/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from app.core.config.settings import DATABASE_ENGINE, DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER
+from app.core.config import settings
 
 
-engine = create_async_engine(f"{DATABASE_ENGINE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}")
+engine = create_async_engine(f"{settings.DATABASE_ENGINE}://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}" f"@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}")
 SessionLocal = async_sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
 
 
